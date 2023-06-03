782ad8dc547f46f4be424138c54a94938d053475
Ever-QN <QuynhTNgo@live.com>
2023-06-02 18:56:38 -0600

    Render gameboard on start button click

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ const Gameboard = (() => {
         gameboard.forEach((square, index) => {
             board += `<div class=square id=square-${index}>${square}</div>`
         })
+        document.querySelector("#gameboard").innerHTML = board;
+        const squares = document.querySelectorAll(".square");
+        squares.forEach((square) => {
+            square.addEventListener("click", handleClick);
+        })
     }
-    document.querySelector("#gameboard").innerHTML = board;
 
     return {
         render,
     }
-})
+})();
 
 const createPlayer = (name, mark) => {
     return {
@@ -24,17 +28,23 @@ const createPlayer = (name, mark) => {
 const Game = (() => {
     let players = [];
     let currentPlayerIndex = 0;
-    let gameOver = false;
+    let gameOver;
 
     const start = () => {
         players = [
             createPlayer(document.querySelector("#player-one").value, "X"),
             createPlayer(document.querySelector("#player-two").value, "O")
         ]
+        currentPlayerIndex = 0;
+        gameOver = false;
+        Gameboard.render();
+    }
+    return {
+        start,
     }
 })();
 
 const startBtn = document.querySelector("#start-button")
 startBtn.addEventListener("click", ()=> {
-    // Game.start();
+    Game.start();
 })
