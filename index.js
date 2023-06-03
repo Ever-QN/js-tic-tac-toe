16284fd765042012481a3ebb812983ec5cf78b96
Ever-QN <QuynhTNgo@live.com>
2023-06-02 18:47:04 -0600

    Add player code to read input

--- index.js
+++ index.js
@@ -14,6 +14,26 @@ const Gameboard = (() => {
     }
 })
 
+const createPlayer = (name, mark) => {
+    return {
+        name,
+        mark
+    }
+}
+
+const Game = (() => {
+    let players = [];
+    let currentPlayerIndex = 0;
+    let gameOver = false;
+
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector("#player-one").value, "X"),
+            createPlayer(document.querySelector("#player-two").value, "O")
+        ]
+    }
+})();
+
 const startBtn = document.querySelector("#start-button")
 startBtn.addEventListener("click", ()=> {
     // Game.start();
